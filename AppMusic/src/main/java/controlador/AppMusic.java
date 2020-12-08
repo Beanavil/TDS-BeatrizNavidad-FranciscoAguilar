c2671b32a7709c0b62c33a8b0349d8b3590e2edf
Beatriz-um <beatriznavidad@yahoo.es>
2020-12-08 11:57:39 +0100

    Clases de persistencia implementadas a falta de los adaptadores y algunas clases del dominio medio implementadas

--- AppMusic/src/main/java/controlador/AppMusic.java
+++ AppMusic/src/main/java/controlador/AppMusic.java
@@ -1,10 +1,10 @@
-package es.um.tds.AppMusic;
+package controlador;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class AppMusic 
 {
     public static void main( String[] args )
     {
